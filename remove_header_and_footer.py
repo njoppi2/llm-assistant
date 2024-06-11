fd9c351421327d6818a85598057b9c6bc747adf0
nelson <njoppi2@gmail.com>
2024-06-11 15:49:14 -0300

    refactor code and fix mid_para bug and and how header-footers are removed from units

--- remove_header_and_footer.py
+++ remove_header_and_footer.py
@@ -10,28 +10,28 @@ def replace_numbers_with_zero(s):
     # Use regular expression to find all numerical substrings
     return re.sub(r'\d+', '0', s)
 
-def process_headers_footers(sorted_units, headers_footers, type_to_process):
+def process_headers_footers(sorted_units):
+    doc_length = len(sorted_units)
     concatenated_units_from_0_to_n_all_per_page = []
+    headers_footers = []
+    biggest_similar_strings = None
+    highest_similar_concatenated_units_number = 0
     
     # Create concatenated units for each page
     for page_units in sorted_units:
         concatenated_page_units = []
         for i in range(len(page_units)):
             concatenated_text = " ".join(page_units[j]['para'] for j in range(i + 1))
+            indexes = [page_units[j]['index'] for j in range(i + 1)]
             concatenated_page_units.append({
                 'page': page_units[0]['page'],
                 'para': concatenated_text,
-                'start_index': 0,
-                'end_index': i
+                'indexes': indexes
             })
         concatenated_units_from_0_to_n_all_per_page.append(concatenated_page_units)
 
-    biggest_similar_strings = None
-    highest_similar_concatenated_units_number = 0
 
     for index, concatenated_page_units_from_0_to_n_per_page in enumerate(zip(*concatenated_units_from_0_to_n_all_per_page)):
-        middle_index = len(concatenated_page_units_from_0_to_n_per_page) // 2
-        middle_para = concatenated_page_units_from_0_to_n_per_page[middle_index]['para']
         
         # Calculate the similarity of the middle concatenated text with the rest
         similar_strings = []
@@ -44,34 +44,33 @@ def process_headers_footers(sorted_units, headers_footers, type_to_process):
             if concatenated_page_units_from_0_to_n['similarity_sum'] / concatenated_page_units_from_0_to_n['count'] > 0.95:
                 similar_strings.append(concatenated_page_units_from_0_to_n)
 
-        doc_length = len(sorted_units)
         minimum_page_number_threshold = doc_length - 5
         total_similar_strings = len(similar_strings)
         if index > 5 and total_similar_strings < minimum_page_number_threshold:
             break
 
         if total_similar_strings >= (minimum_page_number_threshold):
-            total_concatenated_units = len(middle_para.split())
+            total_concatenated_units = len(concatenated_page_units_from_0_to_n['indexes'])
             if total_concatenated_units > highest_similar_concatenated_units_number:
                 highest_similar_concatenated_units_number = total_concatenated_units
                 biggest_similar_strings = similar_strings
 
     if biggest_similar_strings:
-        for unit in biggest_similar_strings:
-            for el in headers_footers:
-                if el['page'] == unit['page']:
-                    for page_unit in sorted_units[unit['page'] - 1][unit['start_index']: unit['end_index'] + 1]:
-                        if page_unit['para'] not in el[type_to_process]:
-                            el[type_to_process].append(page_unit['para'])
+        for bss_page_n in biggest_similar_strings:
+            headers_footers.append({'page': bss_page_n['page'], 'header_footer_indexes': bss_page_n['indexes']})
+
+    return headers_footers
 
 def get_content_without_headers_and_footers(path):
     # Open the PDF file
     doc = fitz.open(path)
+    doc_length = len(doc)
     sorted_header_units = []
     sorted_footer_units = []
     units = []
+    unit_count = 0
     
-    for page_nr in range(len(doc)):
+    for page_nr in range(doc_length):
         page = doc.load_page(page_nr)
         blocks = page.get_text("dict")["blocks"]
         p_height = page.rect.height
@@ -88,8 +87,8 @@ def get_content_without_headers_and_footers(path):
                             line_content.append(paragraph)
                     if len(line_content) >= 0:
                         span = line['spans'][0]
-                        page_units.append({'page': page_nr + 1, 'para': " ".join(line_content), 'x0': span["bbox"][0], 'y0': span["bbox"][1]})
-        
+                        page_units.append({'page': page_nr + 1, 'index': unit_count, 'para': " ".join(line_content), 'x0': span["bbox"][0], 'y0': span["bbox"][1]})
+                        unit_count += 1
         if not page_units:
             continue
         
@@ -108,58 +107,33 @@ def get_content_without_headers_and_footers(path):
         sorted_footer_units.append(footer_area_units)
         sorted_header_units.append(header_area_units)
 
-    headers_footers = [{'page': i + 1, 'header': [], 'footer': []} for i in range(len(doc))]
-
-    process_headers_footers(sorted_footer_units, headers_footers, 'footer')
-    process_headers_footers(sorted_header_units, headers_footers, 'header')
-
-    footers = []
-    headers = []
-
-    for el in headers_footers:
-        counter_f = 0
-        counter_h = 0
-        if el['footer'] and el['header']:
-            for rest in headers_footers:
-                if rest['footer'] and similar(" ".join([f for f in el['footer']]), " ".join([f for f in rest['footer']])) > 0.7:
-                    counter_f += 1
-                if rest['header'] and similar(" ".join([f for f in el['header']]), " ".join([f for f in rest['header']])) > 0.7:
-                    counter_h += 1
-        if counter_f >= len(headers_footers) - 3:
-            footers.append({'page': el['page'], 'footers': el['footer']})
-        if counter_h >= len(headers_footers) - 3:
-            headers.append({'page': el['page'], 'headers': el['header']})
+    footers = process_headers_footers(sorted_footer_units)
+    headers = process_headers_footers(sorted_header_units)
 
     # Remove headers and footers from units
-    content_without_headers_and_footers = []
-    for unit in units:
-        is_header_or_footer = False
-        for hf in headers_footers:
-            if hf['page'] == unit['page']:
-                for header in hf['header']:
-                    if similar(unit['para'], header) > 0.8:
-                        is_header_or_footer = True
-                        break
-                for footer in hf['footer']:
-                    if similar(unit['para'], footer) > 0.8:
-                        is_header_or_footer = True
-                        break
-        if not is_header_or_footer:
-            content_without_headers_and_footers.append(unit)
+    content_without_headers_and_footers = [] # SHOULD SIMPLY BE A NOT IN HEADERS_FOOTERS PER PAGE
+    # just compare what units is to what headers_footers is
+    for page_index in range(doc_length):
+        page_nr = page_index + 1
+        header_indexes_flattened = [index for d in headers if d.get('page') == page_nr for index in d['header_footer_indexes']]
+        footer_indexes_flattened = [index for d in footers if d.get('page') == page_nr for index in d['header_footer_indexes']]
+        header_footer_indexes = [*header_indexes_flattened, *footer_indexes_flattened]
+        units_in_page_n = [u for u in units if u.get('page') == page_nr]
+        content_without_headers_and_footers.extend([u for u in units_in_page_n if u.get('index') not in header_footer_indexes])
 
     # Organize the content back into pages
     organized_content = []
-    for page_nr in range(1, len(doc) + 1):
+    for page_nr in range(1, doc_length + 1):
         page_content = [unit['para'] for unit in content_without_headers_and_footers if unit['page'] == page_nr]
         if page_content:
             organized_content.append({'page': page_nr, 'content': page_content})
 
     return organized_content
 
-for i in range(1, 26):
+for i in range(1, 2):
     print(f"starting {i}")
     path_to_pdf = f'edital{i}.pdf'
-    output_file_name = path_to_pdf + '.txt'
+    output_file_name = path_to_pdf + '_preprocessed2.txt'
     content = get_content_without_headers_and_footers(path_to_pdf)
 
     if os.path.exists(output_file_name):
